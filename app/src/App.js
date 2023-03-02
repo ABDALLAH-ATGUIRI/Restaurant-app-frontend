08334c72505b505efb0a213f6fef92a627900794
ABDALLAH-ATGUIRI <wahbidox@gmail.com>
2023-03-02 15:50:22 +0100

    fix sidebar bage home

--- app/src/App.js
+++ app/src/App.js
@@ -1,11 +1,13 @@
 import logo from "./logo.svg";
 import "./App.css";
 import NavbarComponent from "./components/NavbarComponent";
+import Hero from "./components/Hero";
 
 function App() {
   return (
     <div className="App">
       <NavbarComponent />
+      <Hero />
     </div>
   );
 }
